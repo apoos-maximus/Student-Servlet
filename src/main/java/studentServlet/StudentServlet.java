6fbe9a924f6829bf5b44dce15e34d0f836c0d053
Apoorv Sachan <apoorv.99.sachan@gmail.com>
2020-03-02 13:18:03 +0530

    simple

--- src/main/java/studentServlet/StudentServlet.java
+++ src/main/java/studentServlet/StudentServlet.java
@@ -10,6 +10,7 @@ public class StudentServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doGet(req, resp);
-
+            String reqst = req.getQueryString();
+            resp.getWriter().write(reqst);
     }
 }
